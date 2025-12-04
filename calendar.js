847f711b80f6a05caa961e0ab206f2f90e788c7e
Shincampos <luigi.fiorenzo@gmail.com>
2025-12-04 16:38:00 +0100

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,4 +1,4 @@
-const apiKey = 'patQ4fEuvjEX05PHx.cf31ed791fbac42e2e35d96c0f6551f932b4419eceeb506e7faf20b6f5863b98';
+const apiKey = 'REPLACE_WITH_SERVERLESS'; // non usato nel frontend
 const baseId = 'appajBYutiTd1acc9';
 const tableName = 'Calendario';
 
